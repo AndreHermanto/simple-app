a569d95d4cb4bcbfa140ec7adae8683866606809
andre <andre.hermanto93@gmail.com>
2021-12-16 00:42:06 +0700

    Students template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DOGS_GET_BREEDS_API = 'https://dog.ceo/api/breeds/list/all';
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="show-current-time">{/*show current time*/}</p>
+      <div className="input-lower-case">
+        <p>Check if input are all in lower case</p>
+        <input type='text'></input>
+        {/*replace with your boolean value */true ? 'TRUE' : 'FALSE'}
+      </div>
+      <br />
+      <div className="input-upper-case">
+        <p>Check if input are all in upper case</p>
+        <input type='text'></input>
+        {/*replace with your boolean value */true ? 'TRUE' : 'FALSE'}
+      </div>
+      <div className="input-is-numeric">
+        <p>Check if input is numeric</p>
+        <input type='text'></input>
+        {/*replace with your boolean value */true ? 'TRUE' : 'FALSE'}
+      </div>
+      <br />
+      <div className="simple-form">
+        <label>Firstname:</label>
+        <input type='text' className="firstname"></input>
+        <br/>
+        <label>Lastname:</label>
+        <input type='text' className="lastname"></input>
+        <div className="show-data">
+          <p>Firstname: {/*Replace with a value*/}</p>
+          <p>Lastname: {/*Replace with a value*/}</p>
+        </div>
+      </div>
+      <br />
+      <div className="dog-breeds-list">
+        <ul>{/*Replace with a value*/}</ul>
+      </div>
     </div>
   );
 }
